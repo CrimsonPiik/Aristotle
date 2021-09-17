03e17b31bed46ad90726e42b5e5b4443424e4207
crimsonpiik <moebasem@gmail.com>
2021-09-17 20:28:02 +0300

    fixing

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Aristotle',
-        theme: ThemeData(
-          primarySwatch: Colors.red,
-        ),
-        home: MyHomePage(title: 'Aristotle'),
-        initialRoute: '/menu');
+      debugShowCheckedModeBanner: false,
+      title: 'Aristotle',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Aristotle'),
+    );
   }
 }
 
